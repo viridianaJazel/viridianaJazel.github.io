882a8fa8137a0f318b72cc568bd1f11da30646e7
Viridiana Amezcua <viridiana.j.amezcua.orona@intel.com>
2018-10-08 23:02:20 -0500

    Add bootstrap and font-awesome
    Signed-off-by: Viridiana Amezcua <viridiana.ao.04@gmail.com>

--- src/client/app.js
+++ src/client/app.js
@@ -1,4 +1,13 @@
 import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
+import { fab } from '@fortawesome/free-brands-svg-icons'
+
+import style from './main.scss';
+library.add(fas, far, fab)
 
 export default class App extends Component {
   state = { username: null };
@@ -12,8 +21,9 @@ export default class App extends Component {
   render() {
     const { username } = this.state;
     return (
-      <div>
+      <div className="container">
         {username ? <h1>{`Hello ${username}`}</h1> : <h1>Loading.. please wait!</h1>}
+        <FontAwesomeIcon icon={{ prefix: 'far', iconName: 'smile-beam' }} />
       </div>
     );
   }
--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,25 @@ module.exports = {
         }
       },
       {
+        test: /\.scss$/,
+        loaders: [
+          require.resolve('style-loader'),
+          require.resolve('css-loader'),
+          require.resolve('sass-loader'),
+        ]
+      },
+      {
+        test: /.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+        use: [{
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            outputPath: 'fonts/',    // where the fonts will go
+            publicPath: '../'       // override the default path
+          }
+        }]
+      },
+      {
         test: /\.css$/,
         use: ['style-loader', 'css-loader']
       },
